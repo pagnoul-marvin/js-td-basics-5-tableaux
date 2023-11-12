335e1ce81ddf9232ce41dd86663adb68627802b8
pagnoul-marvin <marvin.pagnoul@student.hepl.be>
2023-11-12 12:42:14 +0100

    2ème partie exercices tableaux

--- starter/script029-2-Array-For.js
+++ starter/script029-2-Array-For.js
@@ -5,18 +5,28 @@
 // EXERCICE 1 :
 // 1. Créez un tableau 'films' contenant les films "Forrest Gump", "Bohemian Rhapsody", "Le Seigneur des anneaux" et "Harry Potter".
 const films = ["Forrest Gump", "Bohemian Rhapsody", "Le Seigneur des anneaux", "Harry Potter"];
-
+let i = 0;
 // 2. Parcourez ce tableau pour afficher les films qu'il contient :
 //    1°) avec une boucle for
 console.log("Avec une boucle for :");
+for (i; i< films.length; i++) {
+    console.log(films[i]);
+}
 
 
 //    2°) avec une boucle while
 console.log("Avec une boucle while :");
+let test = 0;
+while (test<films.length) {
+    console.log(films[test]);
+    test++;
+}
 
 //    3°) avec une boucle for of
 console.log("Avec une boucle for of :");
-
+for (let film of films) {
+    console.log(film);
+}
 
 // EXERCICE 2 :
 // - Stockez le tableau ['John', 'Smith', 1990, 'designer', false, 'blue'] dans une variable 'john'.
@@ -25,12 +35,23 @@ const john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
 // - Parcourez ce tableau pour afficher les informations qu'il contient :
 //    1°) avec une boucle for
 console.log("Avec une boucle for :");
+for (let test2 = 0; test2<john.length; test2++) {
+    console.log(john[test2]);
+}
 
 //    2°) avec une boucle while
 console.log("Avec une boucle while :");
+let test3 = 0;
+while (test3<john.length) {
+    console.log(john[test3]);
+    test3++;
+}
 
 //    3°) avec une boucle for of
-
+console.log("Avec boucle for of : ")
+for (let element of john) {
+    console.log(element);
+}
 
 // EXERCICE 3 :
 // - Créez un tableau "nombres" contenant les 5 premiers entiers.
@@ -38,12 +59,17 @@ const nombres = [1, 2, 3, 4, 5];
 
 // - Affichez "Voici mon tableau de nombres :" suivi du tableau.
 
+console.log(`Voici mon tableau de nombre ${nombres}`);
 
 // - Créez un tableau vide "carres" qui contiendra les carrés de vos nombres.
 const carres = [];
 
 // - Parcourez le tableau "nombres" avec une boucle forEach et ajoutez dans le tableau "carres" le carré de chaque nombre.
 
+nombres.forEach(nombres => {
+    carres.push(nombres*nombres);
+})
 
 // - Affichez "Voici mon tableau de nombres au carré :" suivi du tableau contenant les carrés.
 
+console.log(`Voici mon tableau de nombre au carré ${carres}`);
--- starter/tableaux-p1-mousquetaires.js
+++ starter/tableaux-p1-mousquetaires.js
@@ -10,3 +10,22 @@ TABLEAUX - PRÉPA 1 : Les trois mousquetaires
 6. Utilisez une boucle 'for-of' pour parcourir le tableau et affichez le nom de chaque mousquetaire.
    Précédez le texte "Le pauvre Aramis est mort…".
 */
+let i = 0;
+const mousquetaires = ["Athos", "Porthos", "Aramis"];
+console.log("Voici les 3 mousquetaires :");
+for (i; i < mousquetaires.length; i++) {
+    console.log(mousquetaires[i]);
+}
+
+mousquetaires.push("Artagnan");
+mousquetaires.forEach(mousquetaires =>{
+    console.log(mousquetaires);
+})
+console.log("Ils sont 4 à présent");
+
+mousquetaires.splice(2, 1);
+for (let mousquetaire of mousquetaires) {
+    console.log(mousquetaire);
+
+}
+console.log("Le pauvre Aramis est mort");
